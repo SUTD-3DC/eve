a75ae91f0b863ca25280868a13c18108538ee6ca
yaojie <limyaojie93@gmail.com>
2016-08-02 11:49:29 +0800

    + roboto font

--- modules/clock.js
+++ modules/clock.js
@@ -1,10 +1,16 @@
 function startTime() {
     var today = new Date();
-    var h = today.getHours();
+    var h = today.getHours()
+    var hh = h > 12 ? h - 12 : h;
+    var ampm = h > 12 ? "PM" : "AM"
     var m = today.getMinutes();
-    var s = today.getSeconds();
+    var month = ["January", "Feburary", "March", "April", "May", "June", "July",
+                 "August", "September", "October", "November", "December"];
+
+    // var s = today.getSeconds();
     m = checkTime(m);
-    $('.clock').html(h + ":" + m);
+    $('.date').html(today.getDate() + " " + month[today.getMonth()] + " " + today.getFullYear());
+    $('.time').html(hh + ":" + m + " " + ampm);
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {

c6129920a70e9d3c782ee19cfb4e04f9ca4a44ad
yaojie <limyaojie93@gmail.com>
2016-09-06 17:04:35 +0800

    use wit's speech recognition instead of google due to rotating access tokens

--- index.js
+++ index.js
@@ -34,35 +34,82 @@ function createMainWindow() {
   return win;
 }
 ipcMain.on("getAudioInput", (event) => {
+
+  // google speech relay
+  // exec('rec --encoding signed-integer --bits 16 --channels 1 --rate 16000 out.wav trim 0 3', function(){
+  //   fs.readFile("out.wav", function(err, data) {
+  //     request.post({
+  //       headers: { 'Content-Type': 'application/json', 'Authorization': config.speech.key},
+  //       url: 'https://speech.googleapis.com/v1beta1/speech:syncrecognize',
+  //       json: {
+  //         "config": {
+  //           "encoding":"LINEAR16",
+  //           "sample_rate": 16000
+  //         },
+  //         "audio": {
+  //           "content": data.toString('base64')
+  //         }
+  //       }
+  //     }, function(err, httpResponse, body){
+  //       console.log(httpResponse);
+  //       if (err) {
+  //         return console.error(err);
+  //       }
+  //       fs.unlink("out.wav")
+  //       ipcMain.send("decode", body.results[0].alternatives[0].transcript);
+  //     });
+  //   });
+  // });
+
+  // straight to wit
   exec('rec --encoding signed-integer --bits 16 --channels 1 --rate 16000 out.wav trim 0 3', function(){
     fs.readFile("out.wav", function(err, data) {
       request.post({
-        headers: { 'Content-Type': 'application/json', 'Authorization': config.speech.key},
-        url: 'https://speech.googleapis.com/v1beta1/speech:syncrecognize',
-        json: {
-          "config": {
-            "encoding":"LINEAR16",
-            "sample_rate": 16000
-          },
-          "audio": {
-            "content": data.toString('base64')
-          }
-        }
+        headers: { 'Authorization': config.wit.key,
+                   'Content-Type': 'audio/wav'},
+        url: 'https://api.wit.ai/speech?v=20160902',
+        body: data
       }, function(err, httpResponse, body){
         console.log(httpResponse);
         if (err) {
           return console.error(err);
         }
         fs.unlink("out.wav")
-        ipcMain.send("decode", body.results[0].alternatives[0].transcript);
+        console.log(body);
+        renderResponse(event, JSON.parse(body));
       });
     });
   });
 })
 
-ipcMain.on("decode", (event, message) => {
-  var e = event;
+const renderResponse = (event, response) => {
+  if (response.entities.intent !== null){
+      // there is a valid response
+    if (response.entities.intent[0].value === "weather"){
+      console.log("getting weather..");
+      // get weather
+      // if (body.entities.location !== null){
+        // get weather in location
+      // } else {
+        // get in singapore
+      let url = `https://api.forecast.io/forecast/${config.weather.key}/1.352083,103.819836?units=${config.weather.units}&exclude=minutely,hourly`
+      var request = https.get(url, function(res) {
+        var buffer = "", data, route;
 
+        res.on("data", function(chunk) {
+          buffer += chunk;
+        });
+
+        res.on("end", function(err) {
+          data = JSON.parse(buffer);
+          event.sender.send('weather-reply', data);
+        });
+      });
+    }
+  }
+}
+
+ipcMain.on("decode", (event, message) => {
   request.get(
     { headers: { "Authorization": config.wit.key },
     url: 'https://api.wit.ai/message?v=20160902&q='+message
@@ -72,31 +119,8 @@ ipcMain.on("decode", (event, message) => {
     if (err) {
       return console.error(err);
     }
-    if (response.entities.intent !== null){
-      // there is a valid response
-      if (response.entities.intent[0].value === "weather"){
-        console.log("getting weather..");
-        // get weather
-        // if (body.entities.location !== null){
-          // get weather in location
-        // } else {
-          // get in singapore
-          let url = `https://api.forecast.io/forecast/${config.weather.key}/1.352083,103.819836?units=${config.weather.units}&exclude=minutely,hourly`
-          var request = https.get(url, function(res) {
-            var buffer = "", data, route;
-
-            res.on("data", function(chunk) {
-              buffer += chunk;
-            });
-
-            res.on("end", function(err) {
-              data = JSON.parse(buffer);
-              e.sender.send('weather-reply', data);
-            });
-          });
-        }
-      }
-    });
+    renderResponse(event, response);
+  });
 });
 
 

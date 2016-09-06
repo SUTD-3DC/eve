b3fb0830e8f41c2cb4dc6c881d63240228d721cb
yaojie <limyaojie93@gmail.com>
2016-09-06 16:17:05 +0800

    refactored audio input to main process

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config.js');
 const https = require('https');
 const request = require("request");
 const fs = require("fs");
+const exec = require('child_process').exec;
 
 const app = electron.app;
 const ipcMain = electron.ipcMain;
@@ -32,6 +33,32 @@ function createMainWindow() {
 
   return win;
 }
+ipcMain.on("getAudioInput", (event) => {
+  exec('rec --encoding signed-integer --bits 16 --channels 1 --rate 16000 out.wav trim 0 3', function(){
+    fs.readFile("out.wav", function(err, data) {
+      request.post({
+        headers: { 'Content-Type': 'application/json', 'Authorization': config.speech.key},
+        url: 'https://speech.googleapis.com/v1beta1/speech:syncrecognize',
+        json: {
+          "config": {
+            "encoding":"LINEAR16",
+            "sample_rate": 16000
+          },
+          "audio": {
+            "content": data.toString('base64')
+          }
+        }
+      }, function(err, httpResponse, body){
+        console.log(httpResponse);
+        if (err) {
+          return console.error(err);
+        }
+        fs.unlink("out.wav")
+        ipcMain.send("decode", body.results[0].alternatives[0].transcript);
+      });
+    });
+  });
+})
 
 ipcMain.on("decode", (event, message) => {
   var e = event;
